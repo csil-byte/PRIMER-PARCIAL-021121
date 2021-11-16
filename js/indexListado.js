03d6d010662bee9d33f41f3d743b1643f0ae2570
csil-byte <cgscsilva@gmail.com>
2021-11-16 20:35:35 -0300

    entrega 3

--- js/indexListado.js
+++ js/indexListado.js
@@ -6,10 +6,10 @@ export const crearListadoIndex = (data) => {
 
         const abrirDiv = "<div id='card'>";
         const titulo = "<h2><p>" + element.titulo + "</p></h2>";
-        const descripcion = "<p>" + element.descripcion + "</p>";
+        const descripcion = "<p>Descripcion" + element.descripcion + "</p>";
         const precio = "<p>Precio: " + element.precio + "$</p>";
-        const puertas = "<p>Cantidad Puertas: " + element.nroPuertas + "</p>";
-        const km = "<p>Cantidad Km: " + element.km + "</p>";
+        const puertas = "<p>Cantidad Puertas: " + element.puertas + "</p>";
+        const km = "<p>Cantidad Km: " + element.kms + "</p>";
         const potencia = "<p>Potencia: " + element.potencia + "</p>";
         const cerrarDiv = "</div>";
 
